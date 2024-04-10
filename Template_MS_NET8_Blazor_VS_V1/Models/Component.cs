af9fa50cdd9716763912574ca82a5ddd95d32557
Developer-SLM-com <smatzen@slm.com>
2024-04-09 21:31:17 -0700

    Component data driven Visibility completed.

--- Template_MS_NET8_Blazor_VS_V1/Models/Component.cs
+++ Template_MS_NET8_Blazor_VS_V1/Models/Component.cs
@@ -32,16 +32,24 @@ namespace Template_MS_NET8_Blazor_VS_V1.Models
             c.Add(new Component { Name = "LeftBar", Description = "LeftBar", IsVisible = true });
             c.Add(new Component { Name = "MenuControl", Description = "MenuControl", IsVisible = true });
             c.Add(new Component { Name = "RightBar", Description = "RightBar", IsVisible = true });
-            c.Add(new Component { Name = "StatusBar", Description = "StatusBar", IsVisible = false });
+            c.Add(new Component { Name = "StatusBar", Description = "StatusBar", IsVisible = true });
             c.Add(new Component { Name = "TopMenu", Description = "TopMenu", IsVisible = true });
         }
 
         public string componentStyle(string ComponentName)
         {
             int index = ComponentsCollection.Select((c, i) => new { c, i }).First(x => x.c.Name == ComponentName).i;
-            //int index2 = ComponentsCollection.IndexOf(new Component { Name = ComponentName });
             Component c = ComponentsCollection[index];
             return c.IsVisible ? "display:flex" : "display:none";
+        }
+
+        public string borderStyle(string ComponentName1,string ComponentName2)
+        {
+            int index1 = ComponentsCollection.Select((c, i) => new { c, i }).First(x => x.c.Name == ComponentName1).i;
+            int index2 = ComponentsCollection.Select((c, i) => new { c, i }).First(x => x.c.Name == ComponentName2).i;
+            return ComponentsCollection[index1].IsVisible == true 
+                || ComponentsCollection[index1].IsVisible == true 
+                ? "border: 4px solid darkred" : "border: none";
         }
     }
 
